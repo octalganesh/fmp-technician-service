a9feb032f4c98bd79e243a91770e46445f067da5
HardikOctal <hardik.khurana@octalsoftware.com>
2025-10-06 11:56:07 +0530

    changes for message

--- src/main/java/com/octal/fsm/service/impl/TechnicianServiceImpl.java
+++ src/main/java/com/octal/fsm/service/impl/TechnicianServiceImpl.java
@@ -295,7 +295,7 @@ public class TechnicianServiceImpl implements TechnicianService {
                 }
             });
         } else {
-            throw new CodeException("email does not exist . Please enter a valid email" + email, ErrorCode.BAD_REQUEST);
+            throw new CodeException("email does not exist . Please enter a valid email", ErrorCode.BAD_REQUEST);
         }
     }
 
