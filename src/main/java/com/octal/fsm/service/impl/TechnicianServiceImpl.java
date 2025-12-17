e7c0195c01d708f23d90bdefd02671b6f2c89231
ganeshoctal <ganesh.prajapati@octalsoftware.com>
2025-12-17 18:07:08 +0530

    Add pusEnabled field to MultiUserDeviceDetailsDTO and set it in TechnicianServiceImpl

--- src/main/java/com/octal/fsm/service/impl/TechnicianServiceImpl.java
+++ src/main/java/com/octal/fsm/service/impl/TechnicianServiceImpl.java
@@ -298,6 +298,7 @@ public class TechnicianServiceImpl implements TechnicianService {
                 multiUserDeviceDetailsDTO.setDeviceToken(multiUserDeviceDetails.getDeviceToken());
                 multiUserDeviceDetailsDTO.setDeviceType(multiUserDeviceDetails.getDeviceType());
                 multiUserDeviceDetailsDTO.setAppVersion(multiUserDeviceDetails.getAppVersion());
+                multiUserDeviceDetailsDTO.setPusEnabled(multiUserDeviceDetailsDTO.getPusEnabled());
                 technician.setMultiUserDeviceDetails(multiUserDeviceDetailsDTO);
                 technician.setMultiUserDeviceDetails(multiUserDeviceDetailsDTO);
             }
@@ -729,6 +730,7 @@ public class TechnicianServiceImpl implements TechnicianService {
             dto.setDeviceToken(userDeviceDetails.getDeviceToken());
             dto.setAppVersion(userDeviceDetails.getAppVersion());
             dto.setDeviceId(userDeviceDetails.getDeviceId());
+            dto.setPusEnabled(userDeviceDetails.getPushEnabled());
             deviceDetailsDTOS.add(dto);
         }
         return deviceDetailsDTOS;
