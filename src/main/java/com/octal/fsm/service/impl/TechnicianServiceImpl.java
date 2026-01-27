4f12b961aaa5aa5ecd8ce3b0f8648f9d97ec54d0
kamleshmaurya-creator <kamlesh.maurya@octalsoftware.com>
2026-01-27 11:30:00 +0530

    get user by tenantAndEmail

--- src/main/java/com/octal/fsm/service/impl/TechnicianServiceImpl.java
+++ src/main/java/com/octal/fsm/service/impl/TechnicianServiceImpl.java
@@ -720,15 +720,14 @@ public class TechnicianServiceImpl implements TechnicianService {
         }
     }
 
-    @Override
-    public JobDashboardResponseDTO.Detail countTechnician(JobDashboardResponseDTO.Search search) throws CodeException {
+    public JobDashboardResponseDTO.Detail countTechnician(JobDashboardResponseDTO.Search search,Long tenantId) throws CodeException {
         long count;
         if (search.getStartDate() == null || search.getEndDate() == null) {
 //            throw new CodeException("Start date and end date are required", ErrorCode.COMMON);
-            count = technicianRepository.count();
+            count = technicianRepository.countByTenantId(tenantId);
         }
         else{
-            count = technicianRepository.countByAndCreatedAtBetween(search.getStartDate().atStartOfDay(), search.getEndDate().atTime(23, 59, 59));
+            count = technicianRepository.countByAndCreatedAtBetweenAndTenantId(search.getStartDate().atStartOfDay(), search.getEndDate().atTime(23, 59, 59),tenantId);
         }
         JobDashboardResponseDTO.Detail st = new JobDashboardResponseDTO.Detail();
         st.setTotalNoOfTechnician(count);
